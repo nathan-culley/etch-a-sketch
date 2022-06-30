bdb0a23ffe4981f876de2c5e385208b44cc6b793
Nathan Culley <nathan@nathanculley.com>
2022-06-30 18:42:20 -0400

    Display grid items in grid
    The divs that were being generated to fill the grid are now visible in a 16x16 grid. The number of grid items and numbers of rows and columns are still being set manually. This will be updated in the future to enable user input of grid size.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const wholeGrid = document.getElementById('wholeGrid');
 
-for (i = 0; i < 16; i++) {
-    const gridSquare = document.createElement("div");
+for (i = 0; i < 256; i++) {
+    const gridSquare = document.createElement("p");
     wholeGrid.appendChild(gridSquare);
     gridSquare.classList.add("grid-item");
 }
