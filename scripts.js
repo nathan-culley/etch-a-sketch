f8958936b28c361a02246c39de2996c21dd3a51d
Nathan Culley <nathan@nathanculley.com>
2022-10-19 11:58:00 -0400

    Create basic etch-a-sketch functionality
    Iterated through the array of grid items to add an event listener to each item. This event listener turns the item black when moused over by the user. This creates an etch-a-sketch like effect on the grid. The user does not yet have the ability to generate a grid of their chosen size. There is also an event listener and function that have been commented out that, if enabled, caused the squares to return to their original state after the mouse leaves the square.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,25 @@ for (i = 0; i < 256; i++) {
     wholeGrid.appendChild(gridSquare);
     gridSquare.classList.add("grid-item");
 }
+
+const gridItems = document.getElementsByClassName('grid-item');
+
+Array.from(gridItems).forEach(item => {
+    item.addEventListener('mouseover', (event) => darkenSquare(item));
+})
+
+
+function darkenSquare(item) {
+    item.setAttribute("style", "background-color:black;");
+    console.log("mouseover");
+}
+
+//this event listener and function combination, if enabled, return each square to its original state after the mouse leaves it
+
+/* Array.from(gridItems).forEach(item => {
+    item.addEventListener('mouseout', (event) => lightenSquare(item));
+}) */
+/* function lightenSquare(item) {
+    item.setAttribute("style", "background-color:whitesmoke");
+    console.log("mouseout");
+} */
