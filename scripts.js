8a988f1873ad04c5adee3ecfe71753e73e1d8418
Nathan Culley <nathan@nathanculley.com>
2022-10-19 12:49:37 -0400

    Remove previous grid upon user input
    The generateGrid function now removes the previous grid (if any) before generating the new grid from the user's input, so that the new grid items do not simply get added to the end of the old grid items. The grid's overall size and the number of columns are still hard-set, so there will still be 16 columns no matter what number the user inputs.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,47 @@
-const wholeGrid = document.getElementById('wholeGrid');
+let gridSide = 16;
 
-for (i = 0; i < 256; i++) {
-    const gridSquare = document.createElement("p");
-    wholeGrid.appendChild(gridSquare);
-    gridSquare.classList.add("grid-item");
+//const setGrid = document.getElementById("set-grid");
+//setGrid.eventListener('onclick', )
+
+function setGrid(gridSide) {
+    gridSide = prompt("Enter the number of squares you would like each side of the grid to have:");
+    console.log(gridSide);
+    generateGrid(gridSide);
+    
+} 
+
+generateGrid(gridSide);
+
+function generateGrid(gridSide) {
+    let gridTotal = gridSide * gridSide;
+    console.log(gridSide, gridTotal);
+    //let root = document.documentElement;
+    //root.style.setProperty('--grid-side', gridSide);
+    const wholeGrid = document.getElementById('wholeGrid');
+    //wholeGrid.setAttribute("style", "grid-template-columns:${gridSide}")
+    
+    while (wholeGrid.firstChild) {
+        wholeGrid.removeChild(wholeGrid.lastChild);
+    }
+    
+    for (i = 0; i < gridTotal; i++) {
+        const gridSquare = document.createElement("div");
+        wholeGrid.appendChild(gridSquare);
+        gridSquare.classList.add("grid-item");
+    }
+
+    const gridItems = document.getElementsByClassName('grid-item');
+
+    Array.from(gridItems).forEach(item => {
+        item.addEventListener('mouseover', (event) => darkenSquare(item));
+    })
+
+    function darkenSquare(item) {
+        item.setAttribute("style", "background-color:black;");
+        console.log("mouseover");
 }
-
-const gridItems = document.getElementsByClassName('grid-item');
-
-Array.from(gridItems).forEach(item => {
-    item.addEventListener('mouseover', (event) => darkenSquare(item));
-})
-
-
-function darkenSquare(item) {
-    item.setAttribute("style", "background-color:black;");
-    console.log("mouseover");
 }
-
 //this event listener and function combination, if enabled, return each square to its original state after the mouse leaves it
-
 /* Array.from(gridItems).forEach(item => {
     item.addEventListener('mouseout', (event) => lightenSquare(item));
 }) */
@@ -27,3 +49,4 @@ function darkenSquare(item) {
     item.setAttribute("style", "background-color:whitesmoke");
     console.log("mouseout");
 } */
+
